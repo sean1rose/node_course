10c43c4c1ac900dac6e2215a22d62c8a0f195f99
Sean Rose <sean1rose@gmail.com>
2017-01-26 01:50:05 -0800

    add a few route handlers

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -9,9 +9,27 @@ var app = express();
   // res -> bunch of methods available so u can respond to the request (customize data sent back, set http status codes, etc);
 app.get('/', (req, res) => {
   // send data back...
-  res.send('hey express foo!');
+  // res.send('<h1>Hey FOO!</h1>');
+  // ^ response headers 'content/type' will be 'text/html'
+
+  // to send json back (express automatically formats to json, so response headers 'content/type' will be 'application/json')...
+  res.send({
+    name: 'Sean',
+    likes: ['fantasy football', 'gym']
+  });
 });
 
+// route
+app.get('/about', (req, res) => {
+  res.send('About Page');
+});
+
+// route 
+app.get('/bad', (req, res) => {
+  res.send({
+    error: 'you hit a bad endpoint foo'
+  })
+})
 
 // 3. bind app to a PORT on our machine and LISTEN for requests...
 app.listen(3000);
