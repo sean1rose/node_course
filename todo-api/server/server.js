7f6ff0b53911e793fc2af565b6b92409f6d70c77
Sean Rose <sean1rose@gmail.com>
2017-02-02 01:21:25 -0800

    set up defaults and validators

--- todo-api/server/server.js
+++ todo-api/server/server.js
@@ -4,40 +4,33 @@ mongoose.Promise = global.Promise;
 // configure mongoose and connect to mongodb db 
 mongoose.connect('mongodb://localhost:27017/TodoApp');
 
-// create a model,specifying the attributes we want a todo to have...
+// create a model,specifying the attributes (in object form, aka a SCHEMA) we want a todo to have...
 var Todo = mongoose.model('Todo', {
   text: {
-    type: String
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true
   },
   completed: {
-    type: Boolean
+    type: Boolean,
+    default: false
   },
   completedAt: {
-    type: Number
+    type: Number,
+    default: null
   }
 });
+// schema vs model: http://stackoverflow.com/questions/22950282/mongoose-schema-vs-model
 
 // create a new todo instance
 var newTodo = new Todo({
-  text: 'Cook dinner',
+  text: '  cook dinner      '
 });
+
 
 // save to mongodb database
 newTodo.save().then((doc) => {
-  console.log('Saved todo', doc);
-}, (e) => {
-  console.log('Unable to save todo');
-});
-
-
-var secondTodo = new Todo({
-  text: 'plan wedding',
-  completed: false,
-  completedAt: new Date().getTime()
-});
-
-
-secondTodo.save().then((doc) => {
   console.log(JSON.stringify(doc, undefined, 2));
 }, (e) => {
   console.log('Unable to save todo');
