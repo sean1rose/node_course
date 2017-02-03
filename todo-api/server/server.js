6fe1abc2dfd9d6a5bba652674754b7196db99d3e
Sean Rose <sean1rose@gmail.com>
2017-02-03 00:08:10 -0800

    handle request and response

--- todo-api/server/server.js
+++ todo-api/server/server.js
@@ -18,7 +18,7 @@ app.use(bodyParser.json());
 // can now send json to express app (can access it in route-callback functions)
 
 
-// CONFIGURE ROUTES
+// CONFIGURE ROUTES / ENDPOINTS
 
 // POST /todos
   // send resouce as body -> send (json object) w/ text property to server
@@ -28,16 +28,18 @@ app.post('/todos', (req, res) => {
   // POST HANDLER callback func -> create the todo using the info passed from the user on request.body
   // create todo from request data
   var todo = new Todo({
-    text: req.body.text
+    text: req.body.text,
+    completed: req.body.completed
   });
   // save todo to mongodb using mongoose
   todo.save().then((doc) => {
     // send the saved doc back to the user in response
     res.send(doc);
   }, (e) => {
-    res.send(e);
+    res.status(400).send(e);
   });
 });
+// Can test ^ this post todo endpoint using postman
 
 app.listen(port, () => {
   console.log(`Server is lit on port ${port}`);
